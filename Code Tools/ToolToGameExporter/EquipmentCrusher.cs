d25c7149f1dbf72bb065e3881c7f5c9cb928557d
Paul Fox <p.fox@lorgames.com>
2013-04-23 10:40:06 +0000

    Some fixes?

--- Code Tools/ToolToGameExporter/EquipmentCrusher.cs
+++ Code Tools/ToolToGameExporter/EquipmentCrusher.cs
@@ -5,21 +5,26 @@ using System.Text;
 using ToolCache.Equipment;
 using ToolCache.General;
 using System.Drawing;
+using System.Windows.Forms;
 
 namespace ToolToGameExporter {
     internal class EquipmentCrusher {
 
-        public static EquipmentTypes[] states = { EquipmentTypes.Shadow, EquipmentTypes.Legs, EquipmentTypes.Body, EquipmentTypes.Head, EquipmentTypes.Headgear, EquipmentTypes.Weapon };
+        public static EquipmentTypes[] equipmenttypes = { EquipmentTypes.Shadow, EquipmentTypes.Legs, EquipmentTypes.Body, EquipmentTypes.Head, EquipmentTypes.Headgear, EquipmentTypes.Weapon };
+        public static Direction[] directions = { Direction.Left, Direction.Right, Direction.Up, Direction.Down };
+        public static States[] states = { States.Default, States.Walking, States.Attacking, States.Dancing };
+        public static int[] layers = { 0, 1 };
 
         public static void Go() {
             BinaryIO f = new BinaryIO();
+            short nextID = 0;
             
             //Add the sizes to the things
-            foreach(EquipmentTypes et in states) {
+            foreach(EquipmentTypes et in equipmenttypes) {
                 f.AddShort((short)EquipmentManager.TypeLists[et].Count);
             }
 
-            foreach(EquipmentTypes et in states) {
+            foreach(EquipmentTypes et in equipmenttypes) {
                 //Now throw the equipment into the file as well..?
                 foreach (EquipmentItem ei in EquipmentManager.TypeLists[et]) {
                     f.AddString(ei.Name);
@@ -37,103 +42,86 @@ namespace ToolToGameExporter {
                         f.AddShort(ei.OffsetY_3);
                     }
 
-                    byte defaultsets = 0;
-                    byte walkingsets = 0;
-                    byte attackingsets = 0;
-                    byte dancingsets = 0;
+                    Size size = GetSizeOf(ei);
+                    f.AddByte((byte)size.Width);
+                    f.AddByte((byte)size.Height);
 
-                    defaultsets &= (byte)(ei.Animations[States.Default].Left_0.Frames.Count > 0 ? 1 : 0);
-                    defaultsets &= (byte)(ei.Animations[States.Default].Right_0.Frames.Count > 0 ? 2 : 0);
-                    defaultsets &= (byte)(ei.Animations[States.Default].Up_0.Frames.Count > 0 ? 4 : 0);
-                    defaultsets &= (byte)(ei.Animations[States.Default].Down_0.Frames.Count > 0 ? 8 : 0);
-                    defaultsets &= (byte)(ei.Animations[States.Default].Left_1.Frames.Count > 0 ? 16 : 0);
-                    defaultsets &= (byte)(ei.Animations[States.Default].Right_1.Frames.Count > 0 ? 32 : 0);
-                    defaultsets &= (byte)(ei.Animations[States.Default].Up_1.Frames.Count > 0 ? 64 : 0);
-                    defaultsets &= (byte)(ei.Animations[States.Default].Down_1.Frames.Count > 0 ? 128 : 0);
-
-                    walkingsets &= (byte)(ei.Animations[States.Walking].Left_0.Frames.Count > 0 ? 1 : 0);
-                    walkingsets &= (byte)(ei.Animations[States.Walking].Right_0.Frames.Count > 0 ? 2 : 0);
-                    walkingsets &= (byte)(ei.Animations[States.Walking].Up_0.Frames.Count > 0 ? 4 : 0);
-                    walkingsets &= (byte)(ei.Animations[States.Walking].Down_0.Frames.Count > 0 ? 8 : 0);
-                    walkingsets &= (byte)(ei.Animations[States.Walking].Left_1.Frames.Count > 0 ? 16 : 0);
-                    walkingsets &= (byte)(ei.Animations[States.Walking].Right_1.Frames.Count > 0 ? 32 : 0);
-                    walkingsets &= (byte)(ei.Animations[States.Walking].Up_1.Frames.Count > 0 ? 64 : 0);
-                    walkingsets &= (byte)(ei.Animations[States.Walking].Down_1.Frames.Count > 0 ? 128 : 0);
-
-                    attackingsets &= (byte)(ei.Animations[States.Attacking].Left_0.Frames.Count > 0 ? 1 : 0);
-                    attackingsets &= (byte)(ei.Animations[States.Attacking].Right_0.Frames.Count > 0 ? 2 : 0);
-                    attackingsets &= (byte)(ei.Animations[States.Attacking].Up_0.Frames.Count > 0 ? 4 : 0);
-                    attackingsets &= (byte)(ei.Animations[States.Attacking].Down_0.Frames.Count > 0 ? 8 : 0);
-                    attackingsets &= (byte)(ei.Animations[States.Attacking].Left_1.Frames.Count > 0 ? 16 : 0);
-                    attackingsets &= (byte)(ei.Animations[States.Attacking].Right_1.Frames.Count > 0 ? 32 : 0);
-                    attackingsets &= (byte)(ei.Animations[States.Attacking].Up_1.Frames.Count > 0 ? 64 : 0);
-                    attackingsets &= (byte)(ei.Animations[States.Attacking].Down_1.Frames.Count > 0 ? 128 : 0);
-
-                    dancingsets &= (byte)(ei.Animations[States.Dancing].Left_0.Frames.Count > 0 ? 1 : 0);
-                    dancingsets &= (byte)(ei.Animations[States.Dancing].Right_0.Frames.Count > 0 ? 2 : 0);
-                    dancingsets &= (byte)(ei.Animations[States.Dancing].Up_0.Frames.Count > 0 ? 4 : 0);
-                    dancingsets &= (byte)(ei.Animations[States.Dancing].Down_0.Frames.Count > 0 ? 8 : 0);
-                    dancingsets &= (byte)(ei.Animations[States.Dancing].Left_1.Frames.Count > 0 ? 16 : 0);
-                    dancingsets &= (byte)(ei.Animations[States.Dancing].Right_1.Frames.Count > 0 ? 32 : 0);
-                    dancingsets &= (byte)(ei.Animations[States.Dancing].Up_1.Frames.Count > 0 ? 64 : 0);
-                    dancingsets &= (byte)(ei.Animations[States.Dancing].Down_1.Frames.Count > 0 ? 128 : 0);
-
-                    f.AddByte(defaultsets);
-                    f.AddByte(walkingsets);
-                    f.AddByte(attackingsets);
-                    f.AddByte(dancingsets);
-
-                    if (defaultsets > 0) {
-                        f.AddByte((byte)ei.Animations[States.Default].Left_0.Frames.Count);
-                        f.AddByte((byte)ei.Animations[States.Default].Right_0.Frames.Count);
-                        f.AddByte((byte)ei.Animations[States.Default].Up_0.Frames.Count);
-                        f.AddByte((byte)ei.Animations[States.Default].Down_0.Frames.Count);
-                        f.AddByte((byte)ei.Animations[States.Default].Left_1.Frames.Count);
-                        f.AddByte((byte)ei.Animations[States.Default].Right_1.Frames.Count);
-                        f.AddByte((byte)ei.Animations[States.Default].Up_1.Frames.Count);
-                        f.AddByte((byte)ei.Animations[States.Default].Down_1.Frames.Count);
+                    if (size.Height > 255 || size.Width > 255) {
+                        MessageBox.Show("Size overflow in Equipment crusher. Alert a tool programmer.");
                     }
 
-                    if (walkingsets > 0) {
-                        f.AddByte((byte)ei.Animations[States.Walking].Left_0.Frames.Count);
-                        f.AddByte((byte)ei.Animations[States.Walking].Right_0.Frames.Count);
-                        f.AddByte((byte)ei.Animations[States.Walking].Up_0.Frames.Count);
-                        f.AddByte((byte)ei.Animations[States.Walking].Down_0.Frames.Count);
-                        f.AddByte((byte)ei.Animations[States.Walking].Left_1.Frames.Count);
-                        f.AddByte((byte)ei.Animations[States.Walking].Right_1.Frames.Count);
-                        f.AddByte((byte)ei.Animations[States.Walking].Up_1.Frames.Count);
-                        f.AddByte((byte)ei.Animations[States.Walking].Down_1.Frames.Count);
+                    PackAnimations(ei, nextID, size);
+
+                    //First build an atlas of what animations exist
+                    foreach (States s in states) {
+                        byte stateSets = 0;
+                        nextID = 0;
+
+                        foreach (int l in layers) {
+                            foreach (Direction d in directions) {
+                                nextID++;
+                                stateSets &= (byte)(ei.Animations[s].GetAnimation(d, l).Frames.Count > 0 ? (0x1 << nextID) : 0);
+                            }
+                        }
+
+                        f.AddByte(stateSets);
                     }
 
-                    if (attackingsets > 0) {
-                        f.AddByte((byte)ei.Animations[States.Attacking].Left_0.Frames.Count);
-                        f.AddByte((byte)ei.Animations[States.Attacking].Right_0.Frames.Count);
-                        f.AddByte((byte)ei.Animations[States.Attacking].Up_0.Frames.Count);
-                        f.AddByte((byte)ei.Animations[States.Attacking].Down_0.Frames.Count);
-                        f.AddByte((byte)ei.Animations[States.Attacking].Left_1.Frames.Count);
-                        f.AddByte((byte)ei.Animations[States.Attacking].Right_1.Frames.Count);
-                        f.AddByte((byte)ei.Animations[States.Attacking].Up_1.Frames.Count);
-                        f.AddByte((byte)ei.Animations[States.Attacking].Down_1.Frames.Count);
+                    //Now build an atlas of the frame counts
+                    foreach (States s in states) {
+                        foreach (int l in layers) {
+                            foreach (Direction d in directions) {
+                                if (ei.Animations[s].GetAnimation(d, l).Frames.Count > 0) {
+                                    f.AddByte((byte)ei.Animations[s].GetAnimation(d, l).Frames.Count);
+                                }
+                            }
+                        }
                     }
 
-                    if (dancingsets > 0) {
-                        f.AddByte((byte)ei.Animations[States.Dancing].Left_0.Frames.Count);
-                        f.AddByte((byte)ei.Animations[States.Dancing].Right_0.Frames.Count);
-                        f.AddByte((byte)ei.Animations[States.Dancing].Up_0.Frames.Count);
-                        f.AddByte((byte)ei.Animations[States.Dancing].Down_0.Frames.Count);
-                        f.AddByte((byte)ei.Animations[States.Dancing].Left_1.Frames.Count);
-                        f.AddByte((byte)ei.Animations[States.Dancing].Right_1.Frames.Count);
-                        f.AddByte((byte)ei.Animations[States.Dancing].Up_1.Frames.Count);
-                        f.AddByte((byte)ei.Animations[States.Dancing].Down_1.Frames.Count);
-                    }
+                    nextID++;
                 }
             }
 
             f.Encode(Global.EXPORT_DIRECTORY + "/Equipment_Info.bin");
         }
 
-        public Size GetSizeOf(EquipmentItem ei) {
-            return Size.Empty;
+        private static void PackAnimations(EquipmentItem ei, short nextID, Size s) {
+            Image im = Image.FromFile(t.Animation.Frames[0]);
+            Bitmap bmp = new Bitmap(im.Width * t.Animation.Frames.Count, im.Height, PixelFormat.Format32bppPArgb);
+            Graphics gfx = Graphics.FromImage(bmp);
+
+            im.Dispose();
+
+            gfx.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBilinear;
+            gfx.PixelOffsetMode = System.Drawing.Drawing2D.PixelOffsetMode.HighQuality;
+            gfx.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+
+            for (int i = 0; i < t.Animation.Frames.Count; i++) {
+                im = Image.FromFile(t.Animation.Frames[i]);
+                gfx.DrawImage(im, new Rectangle(im.Width * i, 0, im.Width, im.Height));
+                im.Dispose();
+            }
+
+            bmp.Save(Global.EXPORT_DIRECTORY + "/Equipment_" + nextID + ".png");
+            bmp.Dispose();
+        }
+
+        public static Size GetSizeOf(EquipmentItem ei) {
+            Size s = Size.Empty;
+
+            foreach (KeyValuePair<States, EquipmentAnimationSet> kvp in ei.Animations) {
+                foreach (Direction d in directions) {
+                    foreach (int l in layers) {
+                        for (int i = 0; i < kvp.Value.GetAnimation(d, l).Frames.Count; i++) {
+                            Image im = ImageCache.RequestImage(kvp.Value.GetAnimation(d, l).Frames[i]);
+                            if (s.Width < im.Width) s.Width = im.Width;
+                            if (s.Height < im.Height) s.Height = im.Height;
+                        }
+                    }
+                }
+            }
+
+            return s;
         }
     }
 }
