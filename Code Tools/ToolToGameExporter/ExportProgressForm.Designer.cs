40823919059f3d616ff79d99d97edbdf141812b8
Paul Fox <p.fox@lorgames.com>
2013-06-25 11:17:02 +0000

    Added a cool progress form for exporting so you can see what's happening :)

---
+++ Code Tools/ToolToGameExporter/ExportProgressForm.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace ToolToGameExporter {
+    partial class ExportProgressForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.lblExportTask = new System.Windows.Forms.Label();
+            this.progress = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // lblExportTask
+            // 
+            this.lblExportTask.AutoSize = true;
+            this.lblExportTask.Location = new System.Drawing.Point(12, 14);
+            this.lblExportTask.Name = "lblExportTask";
+            this.lblExportTask.Size = new System.Drawing.Size(125, 13);
+            this.lblExportTask.TabIndex = 0;
+            this.lblExportTask.Text = "Calculating Differences...";
+            // 
+            // progress
+            // 
+            this.progress.Location = new System.Drawing.Point(12, 40);
+            this.progress.Name = "progress";
+            this.progress.Size = new System.Drawing.Size(347, 23);
+            this.progress.TabIndex = 1;
+            // 
+            // ExportProgressForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(371, 75);
+            this.ControlBox = false;
+            this.Controls.Add(this.progress);
+            this.Controls.Add(this.lblExportTask);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ExportProgressForm";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Exporting";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.Label lblExportTask;
+        internal System.Windows.Forms.ProgressBar progress;
+
+    }
+}
--- Code Tools/ToolToGameExporter/Processor.cs
+++ Code Tools/ToolToGameExporter/Processor.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.IO;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace ToolToGameExporter {
     public class Processor {
@@ -11,6 +12,28 @@ namespace ToolToGameExporter {
         public static List<ProcessingError> Errors = new List<ProcessingError>();
 
         public static bool Go(string p, bool silent = false) {
+            Thread t = new Thread(new ParameterizedThreadStart(TaskMon));
+
+            ExportProgressForm epf = new ExportProgressForm();
+
+            InputData d = new InputData();
+            d.p = p;
+            d.silent = silent;
+            d.epf = epf;
+
+            t.Start(d);
+
+            epf.ShowDialog();
+
+            return d.result;
+        }
+
+        private static void TaskMon(object d) {
+            string p = (d as InputData).p;
+            bool silent = (d as InputData).silent;
+            bool result = (d as InputData).result;
+            ExportProgressForm epf = (d as InputData).epf;
+
             Errors.Clear();
 
             if (Directory.Exists(Global.EXPORT_DIRECTORY)) {
@@ -21,38 +44,47 @@ namespace ToolToGameExporter {
 
             //try {
                 //Precrush. No Dependancies.
+                UpdateEPF(epf, "Counting Monsters...", 10);
                 CritterCrusher.Precrush();
 
                 //Tier 0  Crushing. No Dependancies.
+                UpdateEPF(epf, "Amplyfying Sounds...", 20);
                 SoundCrusher.Go();
+                UpdateEPF(epf, "Smashing Objects...", 30);
                 ObjectCrusher.Go();
+                UpdateEPF(epf, "Processing Tiles...", 40);
                 TileCrusher.Go();
+                UpdateEPF(epf, "Crushing Portal...", 50);
                 PortalCrusher.Go();
 
                 //Tier 1 Crushing. Tier 0 Dependancies
+                UpdateEPF(epf, "Converting Equipment...", 60);
                 EquipmentCrusher.Go(); //Requires Sounds.
-                
+
                 //Tier 2 Crushing. Tier 1 Dependancies
+                UpdateEPF(epf, "Exporting Critters...", 70);
                 CritterCrusher.Go(); //Requires Equipment
 
                 //Tier 3 Crushing. Tier 2 Depedancies
+                UpdateEPF(epf, "Exporting Maps...", 80);
                 MapCrusher.Go(); //Requires Portals, Tiles, Sounds and Objects. + Critters
 
+                UpdateEPF(epf, "Copying Data...", 99);
                 if (Directory.Exists(p)) {
                     Directory.Delete(p, true);
                 }
-                
+
                 Directory.Move(Global.EXPORT_DIRECTORY, p);
 
                 if (Errors.Count == 0) {
                     if (!silent) {
                         MessageBox.Show("Exported To Data Folder");
                     }
-                    return true;
+                    ((InputData)d).result = true;
                 } else {
                     FinishedDisplay fd = new FinishedDisplay("Did not export successfully. Errors:", Errors);
                     fd.ShowDialog();
-                    return false;
+                    ((InputData)d).result = false;
                 }
             /*} catch {
                 if(!silent) MessageBox.Show("Please close the exporter and try again! (Some kind of caching issue occurred)");
@@ -61,8 +93,28 @@ namespace ToolToGameExporter {
                     Directory.Delete(Global.EXPORT_DIRECTORY, true);
                 } catch { }
 
-                return false;
+                ((InputData)d).result = false;
             }*/
+
+            UpdateEPF(epf, "", 100);
+
+            epf.Invoke((MethodInvoker)delegate {
+                epf.Close();
+            });
         }
+
+        private static void UpdateEPF(ExportProgressForm epf, string progressLabel, int percent) {
+            epf.Invoke((MethodInvoker)delegate {
+                epf.lblExportTask.Text = progressLabel;
+                epf.progress.Value = percent;
+            });
+        }
+    }
+
+    internal class InputData {
+        public string p;
+        public bool silent;
+        public bool result;
+        public ExportProgressForm epf;
     }
 }
