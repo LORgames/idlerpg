2e60f0f25b56cc9656f085efec732c5124fc177c
Paul Fox <p.fox@lorgames.com>
2013-07-02 08:23:50 +0000

    More UIEditor stuff.

---
+++ Code Tools/ToolCache/UI/UIElement.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ToolCache.UI {
+    public enum UIAnchorPoint {
+        TopLeft,
+        TopCenter,
+        TopRight,
+        MiddleLeft,
+        MiddleCenter,
+        MiddleRight,
+        BottomLeft,
+        BottomCenter,
+        BottomRight
+    }
+
+    public class UIElement {
+        public List<UILayer> Layers = new List<UILayer>();
+        public short OffsetX;
+        public short OffsetY;
+    }
+}
---
+++ Code Tools/ToolCache/UI/UILayer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ToolCache.UI {
+    public enum UILayerType {
+        Overlay,
+        Stretch,
+        StretchToValue
+    }
+
+    public class UILayer {
+        public UILayerType MyType = UILayerType.Overlay;
+        public short Width;
+        public short Height;
+
+        public UILayer() {
+
+        }
+    }
+}
